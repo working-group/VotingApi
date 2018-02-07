3aeacbccfbc5111e05615324987c9bfabbb9a2c3
hichoru <hityorugf@gmail.com>
2018-02-07 02:53:41 +0000

    Update AnswerRepository.php

--- app/Domain/Repository/AnswerRepository.php
+++ app/Domain/Repository/AnswerRepository.php
@@ -1,21 +1,21 @@
-﻿<?php
-2
-3namespace App\Domain\Repository;
-4
-5use DB;
-6
-7/**
-8 * Class AnswerRepository
-9 */
-1class AnswerRepository
-1{  
-1    public function add($answer)
-1    { 
-1        return DB::table('answer')->insertGetId([
-	     'enquete_id'   => $answer->getEnqueteId(),
-	     'item_id'      => $answer->getItemId(),
-	     'comment'      => $answer->getComment(),
-	     'commented_by' => $answer->getCommentedBy(),
-	 )};
+<?php
+
+namespace App\Domain\Repository;
+
+use DB;
+
+/**
+ * Class AnswerRepository
+ */
+class AnswerRepository
+{  
+    public function add($answer)
+    { 
+        return DB::table('answer')->insertGetId([
+             'enquete_id'   => $answer->getEnqueteId(),
+             'item_id'      => $answer->getItemId(),
+             'comment'      => $answer->getComment(),
+             'commented_by' => $answer->getCommentedBy(),
+        )];
 19   }
 21}
