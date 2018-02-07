fc04e8d4779d67ba700be3b3739710c74c9e0582
hichoru <hityorugf@gmail.com>
2018-02-07 02:26:38 +0000

    Create Answer.php

---
+++ app/Domain/Entities/Answer.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Domain\Entities;
+
+/**
+ * Class Answer
+ */
+class Answer
+{
+    private id;
+    private item;
+    private comment;
+    private commentedBy;
+
+    public function __construct(array $array)
+    {
+        $this->id          = $array['id'];
+        $this->item        = $array['item'];
+        $this->comment     = $array['comment'];
+        $this->commentedBy = $array['commentedBy'];
+    }
+    
+    public function getId()
+    {
+        return $this->id;
+    }
+    
+    
+    public function getItem()
+    {
+        return $this->item;
+    }
+    
+    public function getComment()
+    {
+        return $this->comment;
+    }
+    
+    public function getCommentedBy()
+    {
+        return $this->commentedBy;
+    }
+}
