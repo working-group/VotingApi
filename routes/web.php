63f964bd662ac2876ac9324c721b5c2024906cfe
shoooya <shoooya17@gmail.com>
2018-02-07 12:57:01 +0900

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,6 @@ $router->get('/', function () use ($router) {
 
 $router->get('/enquetes', ['uses' => 'EnqueteController@getAll', 'as' => 'enquetes']);
 
-$router->get('/answers/{enquete_id}', ['uses' => 'AnswerController@getByEnqueteId', 'as' => 'answers'])->where(['id' => '[0-9]+']);
+$router->get('/answers/{enquete_id:[0-9]+}', ['uses' => 'AnswerController@getByEnqueteId', 'as' => 'answers']);
 
 $router->post('/answer/{enquete_id}', ['uses' => 'AnswerController@answer', 'as' => 'answer']);
